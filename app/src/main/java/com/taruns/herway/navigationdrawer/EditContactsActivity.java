b3ebdd0a2b65df2eb6ef9edf7411e6ce020a4555
Tarun-108 <tarunstu125@gmail.com>
2023-04-08 00:13:42 +0530

    navigation drawer activities added

---
+++ app/src/main/java/com/taruns/herway/navigationdrawer/EditContactsActivity.java
@@ -0,0 +1,16 @@
+package com.taruns.herway.navigationdrawer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.taruns.herway.R;
+
+public class EditContactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_contacts);
+    }
+}
